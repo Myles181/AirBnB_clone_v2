27b874d0c710d3ab601e282649bdac4013bf2fc3
Myles2005 <cyrile450@gmail.com>
2023-05-19 03:47:16 -0700

    More web pagess

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.strict_slashes=False
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,13 +4,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.strict_slashes=False
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
