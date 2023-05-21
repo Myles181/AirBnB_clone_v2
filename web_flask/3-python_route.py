02eb84369d957c1356ba7c2f03006da7b020141b
Myles2005 <cyrile450@gmail.com>
2023-05-20 18:10:25 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+app.strict_slashes=False
+
+@app.route('/')
+def home():
+    """ Home page
+    Return: Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """ HBNB page
+    Return: HBNB
+    """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    """ C page
+    Args:
+        text -> str()
+    Return: C {text}
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text="is_cool"):
+    """python page
+    Args:
+        text -> str() -> default="is cool"
+        Return: Python {text}
+    """
+    return "Python {}".format(text.replace('_', ' '))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
