85df8ec2eea3aeb20ddecf4b996140270482259d
Myles2005 <cyrile450@gmail.com>
2023-05-20 17:58:03 -0700

    routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,20 +4,30 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.strict_slashes=False
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def home():
-    """ Home Page
+    """ Home page
     Return: Hello HBNB!
     """
     return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """hbnb page
+    """ HBNB page
     Return: HBNB
     """
     return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    """ C page
+    Args:
+        text -> str()
+    Return: C {text}
+    """
+    return "C {}".format(text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
